3165158303adc543dce32adc349fb57f9142ae7c
machua <machuamartin@gmail.com>
2020-11-07 11:47:42 +0300

    added from martinns

--- martinn/settings.py
+++ martinn/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dkl6^shgf#w#v!bf_*t)8-p7!8#0=ke%39%pvvy(a*j6ei1lo1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
